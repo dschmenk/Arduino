ba5dd358c739d3273df37dc870ae0163c93ce879
David Schmenk <dschmenk@MacBook-Pro-2.local>
2015-03-13 11:39:41 -0700

    Check in correct version of processing.py Esplora

--- PyEsplora/EsploraProc.py
+++ PyEsplora/EsploraProc.py
@@ -1,11 +1,8 @@
-add_library('serial')
-
 class Esplora:
     _port = {}
 
     @staticmethod
-    def open():
-        print Serial.list()
+    def open(Serial):
         portName = Serial.list()[len(Serial.list()) - 1] # Pick last serial port in list
         Esplora._port = Serial(this, portName, 57600)
         Esplora._port.clear()
@@ -15,14 +12,25 @@ class Esplora:
         Esplora._port.close()
 
     @staticmethod
+    def readLine(timeout=1000):
+        serstr = None
+        while serstr == None:
+            m = millis()
+            while Esplora._port.available() == 0:
+                if (millis() - m) > timeout:
+                    return ""
+            serstr = Esplora._port.readStringUntil(10)
+        return serstr.strip()
+    
+    @staticmethod
     def readDigital(pin):
         Esplora._port.write("=D%d\n" % pin)
-        return int(Esplora._port.readStringUntil(10).strip())
+        return int(Esplora.readLine())
 
     @staticmethod
     def readAnalog(pin):
         Esplora._port.write("=A%d\n" % pin)
-        return int(Esplora._port.readStringUntil(10).strip())
+        return int(Esplora.readLine())
 
     @staticmethod
     def writeDigital(pin, value):
@@ -52,42 +60,42 @@ class Esplora:
     @staticmethod
     def readJoystick():
         Esplora._port.write("=J\n")
-        instr = Esplora._port.readStringUntil(10).strip().split(',')
-        return [int(instr[0]), int(instr[1])]
+        instr = Esplora.readLine().split(',')
+        return [-int(instr[0]), int(instr[1])]
 
     @staticmethod
     def readAccelerometer():
         Esplora._port.write("=X\n")
-        instr = Esplora._port.readStringUntil(10).strip().split(',')
+        instr = Esplora.readLine().split(',')
         return [int(instr[0]), int(instr[1]), int(instr[2])]
 
     @staticmethod
     def readButtons():
         Esplora._port.write("=B\n")
-        instr = Esplora._port.readStringUntil(10).strip().split(',')
+        instr = Esplora.readLine().split(',')
         return [int(instr[0]), int(instr[1]), int(instr[2]), int(instr[3]), int(instr[4])]
 
     @staticmethod
     def readPotentiometer():
         Esplora._port.write("=P\n")
-        return int(Esplora._port.readStringUntil(10).strip())
+        return int(Esplora.readLine())
 
     @staticmethod
     def readLightSensor():
         Esplora._port.write("=L\n")
-        return int(Esplora._port.readStringUntil(10).strip())
+        return int(Esplora.readLine())
 
     @staticmethod
     def readMicrophone():
         Esplora._port.write("=M\n")
-        return int(Esplora._port.readStringUntil(10).strip())
+        return int(Esplora.readLine())
 
     @staticmethod
     def readTempC():
         Esplora._port.write("=C\n")
-        return int(Esplora._port.readStringUntil(10).strip())
+        return int(Esplora.readLine())
 
     @staticmethod
     def readTempF():
         Esplora._port.write("=F\n")
-        return int(Esplora._port.readStringUntil(10).strip())
+        return int(Esplora.readLine())
