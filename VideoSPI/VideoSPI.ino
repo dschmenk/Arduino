738a75367d8b6759abd05df136db25f38b03d1da
dschmenk <dschmenk@sbcglobal.net>
2015-05-27 15:35:06 -0700

    Add new demo feature (line drawing)

--- VideoSPI/VideoSPI.ino
+++ VideoSPI/VideoSPI.ino
@@ -1,4 +1,4 @@
-/*
+   /*
  * Output NTSC video using the UART in SPI master mode.
  * 40x25 character buffer is rasterized and scanned out in real-time.
  * 320X200 pixels out of 1000 RAM bytes and 1K Flash character ROM.
@@ -280,7 +280,7 @@ void gotoxy(int x, int y)
 //
 // Plot lores pixels
 //
-void setpix(int x, int y)
+void setpix(signed char x, signed char y)
 {
   byte xChar, yChar, blockChar;
   
@@ -291,7 +291,7 @@ void setpix(int x, int y)
   blockChar = 1 << ((x & 1) | ((y & 1) << 1));
   videomem[xChar + yChar * VID_WIDTH] |= blockChar;
 }
-void clrpix(int x, int y)
+void clrpix(signed char x, signed char y)
 {
   byte xChar, yChar, blockChar;
   
@@ -302,7 +302,7 @@ void clrpix(int x, int y)
   blockChar = 1 << ((x & 1) | ((y & 1) << 1));
   videomem[xChar + yChar * VID_WIDTH] &= ~blockChar;
 }
-void xorpix(int x, int y)
+void xorpix(signed char x, signed char y)
 {
   byte xChar, yChar, blockChar;
   
@@ -313,33 +313,113 @@ void xorpix(int x, int y)
   blockChar = 1 << ((x & 1) | ((y & 1) << 1));
   videomem[xChar + yChar * VID_WIDTH] ^= blockChar;
 }
+void line(signed char x1, signed char y1, signed char x2, signed char y2, void (*oppix)(signed char x, signed char y))
+{
+  signed char dx, dy, sx, sy, dx2, dy2, dd2, err;
+  
+  if (x2 >= x1)
+  {
+    dx = x2 - x1;
+    sx = 1;
+  }
+  else
+  {
+    dx = x1 - x2;
+    sx = -1;
+  }
+  if (y2 >= y1)
+  {
+    dy = y2 - y1;
+    sy = 1;
+  }
+  else
+  {
+    dy = y1 - y2;
+    sy = -1;
+  }
+  dx2 = dx << 1;
+  dy2 = dy << 1;
+  if (dx >= dy)
+  {
+    //
+    // X major line
+    //
+    err = dx - dy2;
+    dd2 = dx2 - dy2;
+    while (x1!= x2)
+    {
+      oppix(x1, y1);
+      x1 += sx;
+      if (err < 0)
+      {
+        err += dd2;
+         y1 += sy;
+      }
+      else
+        err -= dy2;
+    }
+  }
+  else
+  {
+    //
+    // Y major line
+    //
+    err = dy - dx2;
+    dd2 = dy2 - dx2;
+    while (y1!= y2)
+    {
+      oppix(x1, y1);
+      y1 += sy;
+      if (err < 0)
+      {
+        err += dd2;
+        x1 += sx;
+      }
+      else
+        err -= dx2;
+    }
+  }
+  oppix(x2, y2);
+}
 /*
  * Run main loop
  */
-byte tab = 0;
-byte xBall = 60;
-byte yBall = 30;
+byte xTab = 2;
+byte yTab = 4;
 signed char  xMove = 1;
 signed char yMove = -1;
+byte xLine = 42;
+byte yLine = 47;
 void loop(void)
 {
   byte i;
 
   if (vblank) // only update every frame
   {
-    gotoxy(tab + 1, tab + 10);
+    gotoxy(xTab + 1, yTab + 10);
     printstr("        ");
-    if (++tab > 10)
-      tab = 0;
-    gotoxy(tab + 1, tab + 10);
-    printstr("Arduino!");
-    xBall = xBall + xMove;
-    if (xBall == 42 || xBall == 77)
+    xTab = xTab + xMove;
+    if (xTab == 0 || xTab == 10)
       xMove = -xMove;
-    yBall = yBall + yMove;
-    if (yBall == 20 || yBall == 47)
+    yTab = yTab + yMove;
+    if (yTab == 0 || yTab == 13 )
       yMove = -yMove;
-    xorpix(xBall, yBall);
+    gotoxy(xTab + 1, yTab + 10);
+    printstr("Arduino!");
+    line(42, 20, xLine, yLine, xorpix);
+    if (xLine == 77 && yLine == 20)
+    {
+      xLine = 42;
+      yLine = 47;
+    }
+    else
+    {
+      if (++xLine > 77)
+      {
+        xLine = 77;
+        yLine--;
+      }
+    }
     vblank=0;
   }
 }
